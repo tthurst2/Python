2c246de70741b68597bcac88ef1f50524f0cd885
Thurston <tylerthurston1@gmail.com>
2018-05-09 13:44:12 -0400

    Added optional command line arguments

--- BrutalWorld/BrutalWorld.py
+++ BrutalWorld/BrutalWorld.py
@@ -1,7 +1,13 @@
 from __future__ import print_function
-import random, time
+import random, time, sys
 
-target_array = list("Hello, World!")
+
+if sys.argv[1:]:
+    target_array = []
+    for x in sys.argv[1:]:
+        target_array += x + " "
+else:
+    target_array = list("Hello, World!")
 string_array = [""] * len(target_array)
 i = 0
 while i < len(target_array):
