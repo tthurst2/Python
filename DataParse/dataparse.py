7eb7e7f69870cd314c3d522c34a926abbfad0e33
Thurston <tylerthurston1@gmail.com>
2018-09-11 12:03:43 -0400

    Made regular expression to match multiple people

--- DataParse/dataparse.py
+++ DataParse/dataparse.py
@@ -2,7 +2,7 @@ import re
 
 inFile = 'Files/userlist.txt'
 outFile = 'Files/userOutput.txt'
-pattern = r'^         (\d{2,4})\s+([A-Z]\S*)\n.*Full Name: ([\w]+.*)\n.*Last Login: (.*) Password Expires: (.*) Multiple Login: (.*)\n\n.*Groups: (.*?)\n'
+pattern = r'.*?         (\d{2,4})\s+([A-Z]\S*)\n.*?Full Name: ([\w]+.*?)\n.*?Last Login: (.*?) Password Expires: (.*?) Multiple Login: (.*?)\n.*?Groups: (.*?\n)'
 new_file =[]
 f = open(inFile, "r")
 text = f.read()
@@ -11,5 +11,5 @@ text = f.read()
 
 outputFile=  re.findall(pattern, text, re.DOTALL)
 print outputFile
-for output in outputFile:
-  print output[0]+ ", " +  output[1].strip() + ", " + output[2].strip() + ", " +  output[3].strip() + ", " +  output[4].strip() + ", " +  output[5].strip() + ", " + output[6]  
+#for output in outputFile:
+  #print output[0]+ ", " +  output[1].strip() + ", " + output[2].strip() + ", " +  output[3].strip() + ", " +  output[4].strip() + ", " +  output[5].strip() + ", " + output[6]  
