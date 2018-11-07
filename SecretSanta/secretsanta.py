2dc69edde732fb1cbc98acfbbd4586c5cb77be66
Thurston <tylerthurston1@gmail.com>
2018-11-07 15:15:02 -0500

    added humans

--- SecretSanta/secretsanta.py
+++ SecretSanta/secretsanta.py
@@ -6,5 +6,6 @@ class Person:
       self.gift1 = None
       self.gift2 = None
 
-p1 = Person("Tyler", 4, 1)
-print p1.name, p1.family, p1.number
+santaList = [Person("Pat", 1, 1), Person("Missy", 1, 2), Person("Lisa", 1, 3), Person("Bernie", 2, 1), Person("Bonnie", 2, 2), Person("Joe", 3, 1), Person("Irene", 3, 2), Person("Alex", 3, 3), Person("Joey", 3, 4), Person("Tyler", 4, 1), Person("Ashley", 5, 1)]
+for x in santaList:
+    print x.name, x.family, x.number
