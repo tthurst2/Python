1aced26d32b074ffada834897f99510e61d6a0bb
Thurston <tylerthurston1@gmail.com>
2018-11-07 15:07:41 -0500

    initialized null gifts

--- SecretSanta/secretsanta.py
+++ SecretSanta/secretsanta.py
@@ -1,10 +1,10 @@
-class MyClass:
-  def __init__(self, name, family, number, gift1, gift2):
+class Person:
+  def __init__(self, name, family, number, gift1 = None, gift2 = None):
       self.name = name
       self.family = family
       self.number = number
       self.gift1 = None
       self.gift2 = None
 
-p1 = MyClass("Tyler", 4, 1, None, None)
+p1 = Person("Tyler", 4, 1)
 print p1.name, p1.family, p1.number
