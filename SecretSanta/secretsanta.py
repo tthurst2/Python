199230666b028d1a6d92eb9155f9948e323e0774
Thurston <tylerthurston1@gmail.com>
2018-11-07 15:24:15 -0500

    added secret santa randomization with no rules

--- SecretSanta/secretsanta.py
+++ SecretSanta/secretsanta.py
@@ -1,3 +1,5 @@
+import random
+
 class Person:
   def __init__(self, name, family, number, gift1 = None, gift2 = None):
       self.name = name
@@ -7,5 +9,15 @@ class Person:
       self.gift2 = None
 
 santaList = [Person("Pat", 1, 1), Person("Missy", 1, 2), Person("Lisa", 1, 3), Person("Bernie", 2, 1), Person("Bonnie", 2, 2), Person("Joe", 3, 1), Person("Irene", 3, 2), Person("Alex", 3, 3), Person("Joey", 3, 4), Person("Tyler", 4, 1), Person("Ashley", 5, 1)]
+
+
+### randomly pick secret santa gifts
+
 for x in santaList:
-    print x.name, x.family, x.number
+    x.gift1 = random.choice(santaList).name
+    x.gift2 = random.choice(santaList).name
+
+###
+
+for x in santaList:
+    print x.name, x.family, x.number, x.gift1, x.gift2
