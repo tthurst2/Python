3efa4bb2f6991bc42d2f1ed8cb4ed9e6bf628974
Thurston <tylerthurston1@gmail.com>
2019-02-26 14:54:12 -0500

    corrected newlines and finished formatting

--- DataParse2/dataparse.py
+++ DataParse2/dataparse.py
@@ -1,8 +1,8 @@
 import re
 
-inFile = 'Files/short.txt'
-outFile = 'Files/shortOutput02262019.txt'
-pattern = r'.*?         (\d{2,4})\s+([A-Z]\S*)\n.*?Full Name: ([\w]+.*?)\n.*?Last Login: (.*?) Password Expires: (.*?) Multiple Login: (.*?)\n.*?Groups: (.*?)\n'
+inFile = 'Files/userlist09192018.txt'
+outFile = 'Files/userOutput09192018.txt'
+pattern = r'.*?      [\* ][\* ][\* ](\d{2,4})\s+([A-Z]\S*)\n.*?Full Name: ([\w]+.*?)\n.*?Last Login: (.*?) Password Expires: (.*?) Multiple Login: (.*?)\n.*?Groups: (.*?\n)'
 new_file =[]
 f = open(inFile, "r")
 #chunk in entire file
@@ -13,5 +13,4 @@ outputFile=  re.findall(pattern, text, re.DOTALL)
 f = open(outFile, "w")
 f.write("ID" + "|" +  "USERNAME" + "|" + "FULL NAME" + "|" + "LAST LOGIN" + "|" + "PW EXPIRES" + "|" + "M. LOGIN" + "|" + "GROUPS" + "\n")
 for output in outputFile:
-  print output
   f.write(output[0]+ "|" +  output[1].strip() + "|" + output[2].strip() + "|" +  output[3].strip() + "|" +  output[4].strip() + "|" +  output[5].strip() + "|" + output[6].replace('|', ','))
